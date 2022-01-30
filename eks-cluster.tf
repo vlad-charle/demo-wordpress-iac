66a729ee00b46daf73a227ba845151cdeeebf220
Vlad Sanyuk <vlad.sanyuk@gmail.com>
2022-01-30 13:01:40 +0200

    Changed: 2 subnets, 2 nodes t3.medium

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -15,14 +15,14 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.micro"
+      instance_type                 = "t3.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 1
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = "t2.micro"
+      instance_type                 = "t3.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
